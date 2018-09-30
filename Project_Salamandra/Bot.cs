d7fadbaa21b5916b88b3168c405d2a864ffa8a1b
triod315 <triod315@gmail.com>
2018-09-30 23:06:36 +0300

    ping added

--- Project_Salamandra/Bot.cs
+++ Project_Salamandra/Bot.cs
@@ -4,19 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telegram.Bot;
-
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace Project_Salamandra
 {
     class Bot
     {
-
         private TelegramBotClient botClient;
 
         private string ConsoleReadToken()
         {
             ConsoleWriter.WriteInputMesage("write token: ");
             string tmp = Console.ReadLine();
+            ConsoleWriter.WritePosMessage("Taken token: " + tmp);
+            return tmp;
+        }
+
+        private string FileReadToken(string fileName)
+        {
+            string tmp=System.IO.File.ReadAllText(fileName);
             ConsoleWriter.WritePosMessage("Taken token: " + tmp);
             return tmp;
         }
@@ -32,7 +39,7 @@ namespace Project_Salamandra
         /// </summary>
         public Bot()
         {
-            token = ConsoleReadToken();
+            token = FileReadToken(@"path");
             InitializeBot();
         }
 
@@ -44,6 +51,11 @@ namespace Project_Salamandra
         {
             this.token = token;
             InitializeBot();
+        }
+
+        ~Bot()
+        {
+            botClient.StopReceiving();
         }
 
         private void InitializeBot()
@@ -70,7 +82,10 @@ namespace Project_Salamandra
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
                 string message = e.Message.Text;
+
+
                 ConsoleWriter.WriteUserMessage(e.Message.Chat.Id + "> " + e.Message.Text);
+
                 if (message == "/time" || message.ToLower().Contains("time"))
                 {
                     botClient.SendTextMessageAsync(e.Message.Chat.Id, DateTime.Now.ToString());
@@ -81,12 +96,32 @@ namespace Project_Salamandra
                     botClient.SendTextMessageAsync(e.Message.Chat.Id, "response \n200");
                     ConsoleWriter.WriteBotMessage("response \n200");
                 }
+                if (message.Substring(0, 4) == "ping")
+                {
+                    string pingResult = PingResult(message.Substring(5, message.Length-5));
+                    botClient.SendTextMessageAsync(e.Message.Chat.Id, pingResult);
+                    ConsoleWriter.WriteBotMessage(pingResult);
+                }
             }
             else
             {
                 ConsoleWriter.WriteNegMessage("Undefined message");
                 botClient.SendTextMessageAsync(e.Message.Chat.Id, "Sorry,but I can't understand you");
             }
+        }
+
+        string PingResult(string strIP)
+        {
+            IPAddress IPaddress = IPAddress.Parse(strIP);
+            Ping ping = new Ping();
+            PingReply pr = ping.Send(IPaddress);
+
+            return "Ping result\n================================\n" +
+                "Time: " + DateTime.Now
+                +"\n================================\n"
+                +"reply time: " + pr.RoundtripTime
+                +"\nstatus: "+pr.Status+"\nIP adders: "
+                +pr.Address+"\n================================\n";
         }
 
     }
@@ -107,7 +142,7 @@ namespace Project_Salamandra
 
         public static void WriteNegMessage(string message) => WriteMessage(message, ConsoleColor.Red);
 
-        public static void WriteBotMessage(string message) => Console.WriteLine("Bot > " + message);
+        public static void WriteBotMessage(string message) => WriteMessage("Bot > " + message,ConsoleColor.Cyan);
 
         public static void WriteUserMessage(string message) => Console.WriteLine("User\\ " + message);
 
--- Project_Salamandra/Program.cs
+++ Project_Salamandra/Program.cs
@@ -3,22 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using Telegram.Bot;
 
 namespace Project_Salamandra
 {
     class Program
-    { 
-      
+    {
+        static Bot salamandra_bot;
         static void Main(string[] args)
         {
-            Bot salamandra_bot = new Bot("674075895:AAEmSJCp2YkSPP1w4j74wMQhSQI5wCD_krE");
+            salamandra_bot = new Bot();
 
             salamandra_bot.Start();
-            Console.ReadLine();
+
+            string command;
+            while (true)
+            { 
+                command=Console.ReadLine();
+                if (command == "stop") break;
+            }
             salamandra_bot.Stop();
         }
 
+        static void WorkingBot()
+        {
+            salamandra_bot.Start();
+        }
          
     }
 }
